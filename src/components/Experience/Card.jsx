28f98a5e24786841ac69a39574f2292713946127
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-22 16:21:59 -0400

    projects page

--- src/components/Experience/Card.jsx
+++ src/components/Experience/Card.jsx
@@ -1,20 +1,22 @@
-import './Card.css'
-import { StarIcon } from '@heroicons/react/24/solid';
+import "./Card.css";
+import { StarIcon } from "@heroicons/react/24/solid";
 
-function Card({job, location, description}) {
-    return (
-        <div className='card'>
-            <div className='deco'>
-                <StarIcon className='star'/>
-                <hr className='line'></hr>
-            </div>
-            <div className='job'>
-                <h2>{job}</h2>
-                <h3 className='location'>{location}</h3>
-                <h3>{description}</h3>
-            </div>
-        </div>
-    );
+function Card({ job, location, description }) {
+  return (
+    <div className="card">
+      <div className="deco">
+        <StarIcon className="star" />
+        <hr className="line"></hr>
+      </div>
+      <div className="job">
+        <h2>{job}</h2>
+        <h3 className="location" id="loc">
+          {location}
+        </h3>
+        <h3 id="exp-descri">{description}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -8,13 +8,34 @@ function Experience() {
                 <h1>experience</h1>
                 <h3>my professional journey and academic milestones</h3>
             </div>
-            <div className="bottom">
+            <div className="bottom-exp">
                 <Card 
-                job='Job Name'
-                location='Location'
-                description='Description'
+                job='Junior Functional Consultant'
+                location='Vffice Inc. (Jan 2025 - May 2025)'
+                description='Supported clients by
+                 analyzing business needs and helping
+                  implement solutions in Microsoft Dynamics 365 Business Central.'
+                />
+                <Card 
+                job='BEng in Software Engineering'
+                location='Concordia University (Jan 2024 - May 2028)'
+                description='Coursework in Data Structure, Algorithms, 
+                Web Development, Software Process and Practices, Operating Systems.'
+                />
+                <Card 
+                job='Cashier'
+                location='Bulkbarn (Oct 2023 - Apr 2024)'
+                description='Provided customer service, 
+                handled transactions and maintained a clean, organized retail environment. '
+                />
+                <Card 
+                job='International Baccalaureate'
+                location='Ecole Secondaire Mosaïque (Sept 2020 - May 2022)'
+                description='HL Physics, HL History of Europe, HL French Literature,
+                 SL Mathematics AA, SL Chemistry, SL English Literature'
                 />
             </div>
+            <hr></hr>
         </section>
     );
 }

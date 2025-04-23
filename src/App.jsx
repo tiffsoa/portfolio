be263096938a2bd2fb85d2220feb186117304f4c
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-23 11:41:08 -0400

    lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Projects from './components/Projects/Projects'
-import Experience from './components/Experience/Experience'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import './App.css'
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Projects";
+import Experience from "./components/Experience/Experience";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import "./App.css";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 function App() {
-
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <>
       <Navbar />
@@ -16,9 +25,8 @@ function App() {
       <Experience />
       <Projects />
       <Contact />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;

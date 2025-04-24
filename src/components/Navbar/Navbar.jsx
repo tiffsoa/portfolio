1ee4b2ca0398c24a937dfe8cf9eeb92e9df84e04
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-24 09:50:39 -0400

    haslink fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,34 @@
-import "./Navbar.css"
+import "./Navbar.css";
+import { HashLink } from "react-router-hash-link";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="navbar-left">
-                <a id="logo" href="#" className="logo">Tiffany Soa A.</a>
-                <a href="#about">about</a>
-                <a href="#experience">experience</a>
-                <a href="#projects">projects</a>
-            </div>
-            <div className="navbar-right">
-                <a href="#contact">contact</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a id="logo" href="#" className="logo">
+          Tiffany Soa A.
+        </a>
+        <HashLink smooth to="#about">
+          about
+        </HashLink>
+        {/* <a href="#about">about</a> */}
+        <HashLink smooth to="#experience">
+          experience
+        </HashLink>
+        {/* <a href="#experience">experience</a> */}
+        <HashLink smooth to="#projects">
+          projects
+        </HashLink>
+        {/* <a href="#projects">projects</a> */}
+      </div>
+      <div className="navbar-right">
+        <HashLink smooth to="#contact">
+          contact
+        </HashLink>
+        {/* <a href="#contact">contact</a> */}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

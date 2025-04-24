a3532bf5d49801ee6f30075a47476054c4f2751c
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-24 11:50:08 -0400

    added transitions, now on titles on about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,34 +1,57 @@
-import './About.css'
+import "./About.css";
+import { useEffect, useRef } from "react";
 
 function About() {
-    return (
-        <section id="about" className="about-section">
-            <div className='top'>
-                <h1 className='title'>about me</h1>
-                <h3 className='descri'>I'm a second year software engineering student
-                    at Concordia University, with a passion for music and science. 
-                    I enjoy learning and discovering new experiences. I also love
-                    designing posters and websites in my free time.
-                </h3>
-            </div>
-            <div className='bottom'>
-                <div className='about-container one'>
-                    <h2>education</h2>
-                    <h3>BEng in Software Engineering, Concordia University<br />
-                    (2024-2028)</h3>
-                </div>
-                <div className='about-container two'>
-                    <h2>skills</h2>
-                    <h3>Java, Python, HTML/CSS, JavaScript</h3>
-                </div>
-                <div className='about-container three'>
-                    <h2>interests</h2>
-                    <h3>Web Development, UI/UX Design, Machine Learning</h3>
-                </div>
-            </div>
-            <hr></hr>
-        </section>
-    );
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    const hiddenEls = sectionRef.current.querySelectorAll(".hidden");
+    hiddenEls.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect(); // cleanup
+  }, []);
+  return (
+    <section id="about" className="about-section" ref={sectionRef}>
+      <div className="top">
+        <h1 className="title hidden">about me</h1>
+        <h3 className="descri hidden">
+          I'm a second year software engineering student at Concordia
+          University, with a passion for music and science. I enjoy learning and
+          discovering new experiences. I also love designing posters and
+          websites in my free time.
+        </h3>
+      </div>
+      <div className="bottom">
+        <div className="about-container one">
+          <h2>education</h2>
+          <h3>
+            BEng in Software Engineering, Concordia University
+            <br />
+            (2024-2028)
+          </h3>
+        </div>
+        <div className="about-container two">
+          <h2>skills</h2>
+          <h3>Java, Python, HTML/CSS, JavaScript</h3>
+        </div>
+        <div className="about-container three">
+          <h2>interests</h2>
+          <h3>Web Development, UI/UX Design, Machine Learning</h3>
+        </div>
+      </div>
+      <hr></hr>
+    </section>
+  );
 }
 
 export default About;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,53 @@
-import heroImage from './tiff.png';
-import './Hero.css'
-import resume from './resume.pdf'
-import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
+import heroImage from "./tiff.png";
+import "./Hero.css";
+import resume from "./resume.pdf";
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+import { useEffect, useRef } from "react";
 
 function Hero() {
-    return (
-        <section id="hero" className="hero-section">
-            <div className="hero-container">
-                <div className="hero-left">
-                    <h1>hi, I'm Tiffany</h1>
-                    <h3>i like software, design, and everything in between</h3>
-                    <div className='hero-resume'>
-                        <a href={resume} target="_blank">
-                            resume
-                            <ArrowUpRightIcon className='hero-arrow'/>
-                        </a>
-                    </div>
-                </div>
-                <div className="hero-right">
-                    <img src={heroImage} alt="a picture of tiffany" className="hero-image"/>
-                </div>
-            </div>
-            <hr/>
-        </section>
-    );
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    const hiddenEls = sectionRef.current.querySelectorAll(".hidden");
+    hiddenEls.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect(); // cleanup
+  }, []);
+
+  return (
+    <section id="hero" className="hero-section" ref={sectionRef}>
+      <div className="hero-container">
+        <div className="hero-left">
+          <h1 className="hero-text hidden">hi, I'm Tiffany</h1>
+          <h3 className="hero-text hidden">i like software, design, and everything in between</h3>
+          <div className="hero-resume hero-text hidden">
+            <a href={resume} target="_blank">
+              resume
+              <ArrowUpRightIcon className="hero-arrow" />
+            </a>
+          </div>
+        </div>
+        <div className="hero-right">
+          <img
+            src={heroImage}
+            alt="a picture of tiffany"
+            className="hero-image"
+          />
+        </div>
+      </div>
+      <hr />
+    </section>
+  );
 }
 
 export default Hero;

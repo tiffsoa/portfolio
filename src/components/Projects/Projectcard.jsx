cd92d9e5f09a2a5adc8963a14d1daf8c902bfb01
Tiffany Andriamiharimanana <atiff.soa@gmail.com>
2025-04-24 14:00:40 -0400

    fix

--- src/components/Projects/Projectcard.jsx
+++ src/components/Projects/Projectcard.jsx
@@ -3,21 +3,23 @@ import "./Projectcard.css"
 
 function Projectcard({name, tech, github}) {
     return (
-        <a className="project-link" href={github} target="_blank">
+        
             <div className="project-card">
-            <FolderIcon id="folder"/>
-            <h2 id="project-name">{name}</h2>
-            <div className="tech-project">
-                {tech.map((item, index) =>(
-                    <span key={index} className="tech-stack">
-                        {item}
-                    </span>
-                ))}
+                <a className="project-link" href={github} target="_blank">
+                    <FolderIcon id="folder"/>
+                    <h2 id="project-name">{name}</h2>
+                    <div className="tech-project">
+                    {tech.map((item, index) =>(
+                        <span key={index} className="tech-stack">
+                            {item}
+                        </span>
+                     ))} </div>
+                </a>
             </div>
 
 
-        </div>
-        </a>
+            
+        
     );
 }
 
